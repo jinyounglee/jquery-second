68c39bb0e451d8044e8f955aeafe94d377618de2
jinyoung123 <hs25823@naver.com>
2015-09-12 20:45:15 +0900

    Commit Test

--- js/login.js
+++ js/login.js
@@ -1,13 +1,17 @@
 // js 파일 로
 $(function(){
-	$('#btnLogin').click(showEmail);
-	$('font').hide();
+	$('#btnLogin').click( showEmail );
+	$('font').hide(); 
 });
 
 
 function showEmail(){
+
 	var email = $('#inputEmail').val();
-	var password = $('#inputPassword').val('test'); // setter 로서의 역할만 한다. 
-	var password1 = $('#inputPassword').val();
-	alert('email :: ' + email + '\n' + 'password :: ' + password + '\n' + 'password1 :: ' + password1 );
+	var password = $('#inputPassword').val(); // setter 로서의 역할만 한다. 
+
+	if( !email ){
+		$('font').show();
+	}
+	
 }
